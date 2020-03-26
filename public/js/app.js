895f0aad81da2642635c490839dcdb795d14d361
M <m@Ms-MacBook-Pro.local>
2020-03-26 23:07:04 +0300

    new log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('client side script is loaded')
+console.log('this log was changed')
 
 // fetch('http://puzzle.mead.io/puzzle').then( (response) => {
 //     response.json().then( (data) => {
